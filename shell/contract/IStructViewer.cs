e0fe986afd52c5d3728afba569b975c7d268a9f1
Y-Theta <yt0104150455@outlook.com>
2025-10-24 20:35:30 +0800

    reform

--- shell/contract/IStructViewer.cs
+++ shell/contract/IStructViewer.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace learning_tools.contract
 {
     internal interface IStructViewer
     {
+        string DisplayName { get; }
+
+        void Display(object datas);
     }
 }
---
+++ shell/views/TreeViewer.xaml.cs
@@ -0,0 +1,44 @@
+﻿using learning_tools.contract;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace learning_tools.views
+{
+    /// <summary>
+    /// UserControl1.xaml 的交互逻辑
+    /// </summary>
+    public partial class TreeViewer : UserControl, IStructViewer
+    {
+        public TreeViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadTree()
+        {
+
+        }
+
+        #region   IStructViewer
+        public string DisplayName => nameof(TreeViewer);
+
+        public void Display(object datas)
+        {
+        }
+        #endregion
+
+    }
+}
