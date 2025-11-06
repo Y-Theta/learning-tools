6b85f0ff9a672894e07f5804f15280bae104745f
杨杰铭 <yangjm@mctech.vip>
2025-11-06 19:40:16 +0800

    add unifind

---
+++ base/Contract.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace commonstruct
+{
+    public interface IUniFindAdapter<T>
+    {
+        void SetParent(T item, T parent);
+
+        T GetParent(T item);
+    }
+}
---
+++ base/UniFind.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace commonstruct
+{
+    public class UniFind<T>
+    {
+        public UniFind() { }
+
+        public void Union(T a, T b) { }
+
+        public T this[T item]
+        {
+            get
+            {
+                return default;
+            }
+        }
+
+    }
+}
